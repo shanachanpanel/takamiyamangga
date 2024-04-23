34809cce9bed380c377750f60b8f2f1694e734e8
shanachan xixixi <126304667+shanachanpanel@users.noreply.github.com>
2024-04-23 16:10:20 +0700

    Update [manhwa].jsx

--- pages/komikk/manhwa/page/[manhwa].jsx
+++ pages/komikk/manhwa/page/[manhwa].jsx
@@ -13,7 +13,7 @@ export default function Manhwa() {
 
   async function setDaftar(page = 1) {
     const res = await fetch(
-      "https://komi.katowproject.app/api/komikindo/komikk/manhwa/page/" + page
+      "https://ranimev2-api.vercel.app/komiku/manhwa/page/" + page
     );
     const data = await res.json();
     setPage(data.mangas);
